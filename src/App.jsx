38af7fde183356eeb70d044ae79c54d327014baf
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-03-05 13:34:34 +0200

    total number of click is added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ function App() {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
+    console.log('left before', left)
     setLeft(left + 1)
+    console.log('left after', left)
+    setTotal(left + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
+    setTotal(left + right)
   }
 
   return (
@@ -22,6 +27,7 @@ function App() {
       <button onClick={handleRightClick}>right</button>
       {right}
       <p>{allClicks.join(' ')}</p>     // new empty space is added with every button click
+      <p>Total {total}</p>
     </div>
   )
 }
